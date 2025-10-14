8326df9c45fd88efc3a40dbf32af7a321cdb8114
Joaquin Orellana <joaquinosotelo@gmail.com>
2025-10-13 21:37:25 -0300

    Subimos los assets para que los tengan los compañeros

---
+++ AR reconocer objetos/ARTapies_prototype/Assets/GameController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject cuboA;
+    public GameObject cuboB;
+    public GameObject cuboC;
+    public GameObject cuboD;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
